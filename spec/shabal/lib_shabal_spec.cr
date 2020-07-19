3934cffa4daf7d9326c720cd3d98f72c2a2e281b
zyzzyva <zyzzyva99@gmail.com>
2020-07-19 12:49:54 -0400

    A little code cleanup

--- spec/shabal/lib_shabal_spec.cr
+++ spec/shabal/lib_shabal_spec.cr
@@ -1,17 +1,17 @@
 require "../spec_helper"
 
-describe "LibShabal" do
+describe "Shabal::LibShabal" do
   it "should correctly sign and verify a message digest with curve25519" do
     message_digest = sha256_hash("Let's see if this works...".to_slice)
     private_key = sha256_hash("My completely unguessable passphrase".to_slice)
 
     public_key_ptr = Pointer(UInt8).malloc(32)
-    LibShabal.curve25519_get_public_key(private_key.to_unsafe, public_key_ptr)
+    Shabal::LibShabal.curve25519_get_public_key(private_key.to_unsafe, public_key_ptr)
 
     signature_ptr = Pointer(UInt8).malloc(64)
-    LibShabal.curve25519_sign(private_key.to_unsafe, message_digest.to_unsafe, signature_ptr)
+    Shabal::LibShabal.curve25519_sign(private_key.to_unsafe, message_digest.to_unsafe, signature_ptr)
 
-    result = LibShabal.curve25519_verify(public_key_ptr, signature_ptr, message_digest.to_unsafe, 1)
+    result = Shabal::LibShabal.curve25519_verify(public_key_ptr, signature_ptr, message_digest.to_unsafe, 1)
 
     result.should_not eq(0)
   end
--- src/shabal/lib_shabal.cr
+++ src/shabal/lib_shabal.cr
@@ -1,74 +1,76 @@
-@[Link("shabal")]
-lib LibShabal
-  type UIntPtrT = LibC::ULong
+module Shabal
+  @[Link("shabal")]
+  lib LibShabal
+    type UIntPtrT = LibC::ULong
 
-  fun create_plot(
-    account_id : UInt64,
-    nonce : UInt64,
-    poc_version : UInt8,
-    plot_buffer : UInt8*,
-    plot_buffer_offset : UIntPtrT
-  ) : Void
-  fun create_plots(
-    account_id : UInt64,
-    start_nonce : UInt64,
-    nonce_count : UInt64,
-    poc_version : UInt8,
-    plot_buffer : UInt8*,
-    plot_buffer_offset : UIntPtrT
-  ) : Void
-  fun create_scoop(
-    account_id : UInt64,
-    nonce : UInt64,
-    scoop : UInt32,
-    poc_version : UInt8,
-    sccop_buffer : UInt8*,
-    sccop_buffer_offset : UIntPtrT
-  ) : Void
+    fun create_plot(
+      account_id : UInt64,
+      nonce : UInt64,
+      poc_version : UInt8,
+      plot_buffer : UInt8*,
+      plot_buffer_offset : UIntPtrT
+    ) : Void
+    fun create_plots(
+      account_id : UInt64,
+      start_nonce : UInt64,
+      nonce_count : UInt64,
+      poc_version : UInt8,
+      plot_buffer : UInt8*,
+      plot_buffer_offset : UIntPtrT
+    ) : Void
+    fun create_scoop(
+      account_id : UInt64,
+      nonce : UInt64,
+      scoop : UInt32,
+      poc_version : UInt8,
+      sccop_buffer : UInt8*,
+      sccop_buffer_offset : UIntPtrT
+    ) : Void
 
-  fun curve25519_get_public_key(private_key : UInt8*, public_key : UInt8*) : Void
-  fun curve25519_get_shared_secret(
-    private_key : UInt8*,
-    public_key : UInt8*,
-    shared_secret_buffer : UInt8*
-  ) : Void
-  fun curve25519_sign(
-    private_key : UInt8*,
-    message_sha256 : UInt8*,
-    signature_buffer : UInt8*
-  ) : Void
+    fun curve25519_get_public_key(private_key : UInt8*, public_key : UInt8*) : Void
+    fun curve25519_get_shared_secret(
+      private_key : UInt8*,
+      public_key : UInt8*,
+      shared_secret_buffer : UInt8*
+    ) : Void
+    fun curve25519_sign(
+      private_key : UInt8*,
+      message_sha256 : UInt8*,
+      signature_buffer : UInt8*
+    ) : Void
 
-  fun curve25519_verify(
-    public_key : UInt8*,
-    signature : UInt8*,
-    message_sha256 : UInt8*,
-    enforce_canonical : UInt8
-  ) : UInt8
+    fun curve25519_verify(
+      public_key : UInt8*,
+      signature : UInt8*,
+      message_sha256 : UInt8*,
+      enforce_canonical : UInt8
+    ) : UInt8
 
-  fun libshabal_version : UInt8*
+    fun libshabal_version : UInt8*
 
-  fun shabal256_destroy(shabal : Void*) : Void
-  fun shabal256_digest(shabal : Void*, buffer : UInt8*, offset : UIntPtrT) : Void
-  fun shabal256_new : Void*
-  fun shabal256_reset(shabal : Void*) : Void
-  fun shabal256_update(
-    shabal : Void*,
-    data : UInt8*,
-    offset : UIntPtrT,
-    len : UIntPtrT
-  ) : Void
+    fun shabal256_destroy(shabal : Void*) : Void
+    fun shabal256_digest(shabal : Void*, buffer : UInt8*, offset : UIntPtrT) : Void
+    fun shabal256_new : Void*
+    fun shabal256_reset(shabal : Void*) : Void
+    fun shabal256_update(
+      shabal : Void*,
+      data : UInt8*,
+      offset : UIntPtrT,
+      len : UIntPtrT
+    ) : Void
 
-  fun shabal_findBestDeadline(
-    scoops : UInt8*,
-    nonce_count : UInt64,
-    gensig : UInt8*
-  ) : UInt64
-  fun shabal_findBestDeadlineDirect(
-    scoops : UInt8*,
-    nonce_count : UInt64,
-    gensig : UInt8*,
-    best_deadline : UInt64*,
-    best_offset : UInt64*
-  ) : Void
-  fun shabal_init : Void
+    fun shabal_findBestDeadline(
+      scoops : UInt8*,
+      nonce_count : UInt64,
+      gensig : UInt8*
+    ) : UInt64
+    fun shabal_findBestDeadlineDirect(
+      scoops : UInt8*,
+      nonce_count : UInt64,
+      gensig : UInt8*,
+      best_deadline : UInt64*,
+      best_offset : UInt64*
+    ) : Void
+    fun shabal_init : Void
+  end
 end
