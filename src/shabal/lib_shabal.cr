1d0aa6c31cc8e28f278dd43b04d69513720af4a3
zyzzyva <zyzzyva99@gmail.com>
2020-07-19 12:38:51 -0400

    Remove strings copied from libshabal.h so we can use MIT license

--- src/shabal/lib_shabal.cr
+++ src/shabal/lib_shabal.cr
@@ -1,12 +1,7 @@
-# Note that docstrings are copied from libshabal.h, where available.
-
 @[Link("shabal")]
 lib LibShabal
   type UIntPtrT = LibC::ULong
 
-  # Creates a single PoC Nonce.
-  #
-  # `plot_buffer` must be correct size - no size checks are performed.
   fun create_plot(
     account_id : UInt64,
     nonce : UInt64,
@@ -14,12 +9,6 @@ lib LibShabal
     plot_buffer : UInt8*,
     plot_buffer_offset : UIntPtrT
   ) : Void
-
-  # Creates PoC Nonces, with SIMD instructions for extra speed.
-  #
-  # `plot_buffer` must be correct size - no size checks are performed.
-  #
-  # `nonce_count` counts from 1 - 0 is no plots and will do nothing.
   fun create_plots(
     account_id : UInt64,
     start_nonce : UInt64,
@@ -28,10 +17,6 @@ lib LibShabal
     plot_buffer : UInt8*,
     plot_buffer_offset : UIntPtrT
   ) : Void
-
-  # Creates a single PoC Scoop.
-  #
-  # `plot_buffer` must be correct size - no size checks are performed.
   fun create_scoop(
     account_id : UInt64,
     nonce : UInt64,
@@ -53,8 +38,6 @@ lib LibShabal
     signature_buffer : UInt8*
   ) : Void
 
-  # `enforce_canonical` and the return value are u8 bools where 0 represents false and 1 represents true.
-  # So, a return value of 1 indicates successful verification and a return value of 0 indicates unsuccessful verification.
   fun curve25519_verify(
     public_key : UInt8*,
     signature : UInt8*,
@@ -64,38 +47,10 @@ lib LibShabal
 
   fun libshabal_version : UInt8*
 
-  # Destroy a Shabal256 instance, clearing memory allocated for it.
-  #
-  # `shabal` is the pointer to the instance returned from `shabal256_new()`
   fun shabal256_destroy(shabal : Void*) : Void
-
-  # Retrieve the result of a Shabal256 digest and reset the digest.
-  #
-  # Stores the data in `buffer` starting from `offset`. Stores 32 bytes of hash data.
-  #
-  # `buffer` must have 32 bytes available from `offset` otherwise this will attempt to write beyond the array.
   fun shabal256_digest(shabal : Void*, buffer : UInt8*, offset : UIntPtrT) : Void
-
-  # Create a new Shabal256 instance
-  #
-  # Returns a pointer to the instance, which
-  # can be used with the other functions to
-  # manipulate the instance.
-  #
-  # This implementation of Shabal256 is not particularly
-  # fast but is here for completeness.
   fun shabal256_new : Void*
-
-  # Reset a Shabal256 instance to its initial state
-  #
-  # `shabal` is the pointer to the instance returned from `shabal256_new()`
   fun shabal256_reset(shabal : Void*) : Void
-
-  # Update a Shabal256 instance with new input data
-  #
-  # `shabal` is the pointer to the instance returned from `shabal256_new()`
-  #
-  # Inputs data into the digest from `data` starting at `offset` of length `len`
   fun shabal256_update(
     shabal : Void*,
     data : UInt8*,
